76c12087c737ccce93a8d991cbaabaedc86aebc8
uslsteen <shurygin0171@gmail.com>
2023-05-31 17:26:23 +0300

    [custom_pass] pass was fixed

--- llvm/lib/Transforms/InstrCounter/InstrCounter.cpp
+++ llvm/lib/Transforms/InstrCounter/InstrCounter.cpp
@@ -1,56 +1,88 @@
-//
 #include "llvm/ADT/Statistic.h"
 #include "llvm/IR/Function.h"
-#include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/raw_ostream.h"
-#include "llvm/Transforms/IPO/PassManagerBuilder.h"
-//
+
 #include <iostream>
 #include <map>
-//
+#include <string>
+
 using namespace llvm;
 
-//
+#define DEBUG_TYPE "hello"
+
+STATISTIC(InstrCounter, "Counts number of opcodes");
+
 namespace {
-struct MnCounterPass : public FunctionPass {
-  //
+//
+struct InstrCounterPass : public FunctionPass {
+  static char ID; // Pass identification, replacement for typeid
   std::map<std::string, int> mn_counter;
-  static char ID;
 
-  //
-  MnCounterPass() : FunctionPass(ID) {}
+  InstrCounterPass() : FunctionPass(ID) {}
 
-  //
-  virtual bool runOnFunction(Function &F) {
-    //
-    outs() << "Step into function " << F.getName() << '\n';
+  bool runOnFunction(Function &F) override {
+    errs() << "Step into function " << F.getName() << '\n';
     //
     for (auto &B : F)
       for (auto &I : B)
         mn_counter[I.getOpcodeName()]++;
 
     for (auto &&it : mn_counter)
-      outs() << it.first << " : " << it.second << "\n";
+      errs() << it.first << " : " << it.second << "\n";
     //
-    outs() << "\n";
+    errs() << "\n";
     mn_counter.clear();
-    //
-    return true;
+    return false;
   }
 };
 } // namespace
 
-char MnCounterPass::ID = 0;
-static RegisterPass<MnCounterPass> X("icount",
-                                     "Counts mnemonics per functions");
+char InstrCounterPass::ID = 0;
+static RegisterPass<InstrCounterPass> X("icounter", "Instruction counter Pass");
 
-// static void RegisterInstrCounterPass(const PassManagerBuilder &,
-//                                      legacy::PassManagerBase &PM) {
-//   PM.add(new MnCounterPass());
-// }
-// 
 // //
-// static RegisterStandardPasses
-//     RegisterMyPass(PassManagerBuilder::EP_EarlyAsPossible,
-//                    RegisterInstrCounterPass);
+// #include "llvm/ADT/Statistic.h"
+// #include "llvm/IR/Function.h"
+// #include "llvm/IR/LegacyPassManager.h"
+// #include "llvm/Pass.h"
+// #include "llvm/Support/raw_ostream.h"
+// #include "llvm/Transforms/IPO/PassManagerBuilder.h"
+// //
+// #include <iostream>
+// #include <map>
+// //
+// using namespace llvm;
+//
+// //
+// namespace {
+// struct MnCounterPass : public FunctionPass {
+//   //
+//   std::map<std::string, int> mn_counter;
+//   static char ID;
+//   //
+//   MnCounterPass() : FunctionPass(ID) {}
+//
+//   //
+//   bool runOnFunction(Function &F) override {
+//     //
+//     errs() << "Step into function " << F.getName() << '\n';
+//     //
+//     for (auto &B : F)
+//       for (auto &I : B)
+//         mn_counter[I.getOpcodeName()]++;
+//
+//     for (auto &&it : mn_counter)
+//       errs() << it.first << " : " << it.second << "\n";
+//     //
+//     errs() << "\n";
+//     mn_counter.clear();
+//     //
+//     return true;
+//   }
+// };
+// } // namespace
+//
+// char MnCounterPass::ID = 0;
+// static RegisterPass<MnCounterPass> X("icount",
+//                                      "Counts mnemonics per functions");
